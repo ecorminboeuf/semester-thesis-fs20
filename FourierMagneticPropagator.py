61f51869c12943bb52769270f31eb265fca708e3
cetienne <cetienne@student.ethz.ch>
2020-07-20 10:26:01 +0200

    updated bibliography, corrected grammar

--- FourierMagneticPropagator.py
+++ FourierMagneticPropagator.py
@@ -168,23 +168,24 @@ class FourierMagneticPropagator(Propagator, SplittingParameters):
 
 
     def post_propagate(self, tspan):
-        r"""Given an initial wavepacket :math:`\Psi_0` at time :math:`t=0`, calculate the propagated wavepacket :math:`\Psi` at time :math:`tspan \[ 0 \]`. We perform :math:`n = \lceil tspan\[ 0 \] /dt \rceil` steps of size :math:`dt`.
+        r"""Given an initial wavepacket :math:`\Psi_0` at time :math:`t=0`, calculate the propagated wavepacket :math:`\Psi` at time :math:`tspan [0]`. We perform :math:`n = \lceil tspan[ 0 ] /dt \rceil` steps of size :math:`dt`.
 
-        :param tspan: :py class:`ndarray` consisting of end time at position 0, other positions are irrelevant.
+        :param tspan: ``ndarray`` consisting of end time at position 0, other positions are irrelevant.
         """
 
-        # (ignoriere tspan[0])
+        #Define stepwidth
         nsteps = int(tspan[0] / self._dt + 0.5)
+        #Console output
         print("Perform " + str(nsteps) + " steps from t = 0.0 to t = " + str(tspan[0]))
 
 
-        # Magnetfeld Matrix B(t)
+        # Define magnetic field matrix B(t)
         B = lambda t: self._B(t)
 
         #how many components does Psi have
         N = self._psi.get_number_components()
 
-        #start time t_0 = 0?
+        #set start time and time grids
         t0 = 0
         t_a = t0
         t_b = t0
@@ -203,60 +204,78 @@ class FourierMagneticPropagator(Propagator, SplittingParameters):
         values = tuple([val.reshape(self._grid.get_number_nodes()) for val in values])
         self._psi.set_values(values)
 
+        # rotate grid back to original orientation
         self._grid.rotate(R)
 
-        #calculate the necessary timesteps
+        #calculate timesteps
+        # each j is an individual time steps
         for j in range(nsteps):
+            # each i is an intermediate time step in the splitting
             for i in range(len(self._a)):
-                # Integral -\int_{tspan[0]}^{tspan[1]}B^2(s)ds und zugehörige Propagation
-                # (siehe Paper, Remark 3.1)
+
+                ### Calculate potential flow map \Phi_V = \Phi_B * \Phi_{\phi}###
+
+                # Integral -\int_{tspan[0]}^{tspan[1]}B^2(s)ds and its associated propagation
+                # does not need to be rotated, as B^2(s) is independent of rotations
+                # (see [Gradinaru and Rietmann, 2020]. Remark 3.1)
                 minus_B_squared = lambda t: (-1.0) * dot(B(t), B(t))
                 A = 1.0 / 8.0 * MagneticField.matrix_quad(minus_B_squared, t_a, t_a + self._a[i]*self._dt)
 
                 X = self._grid.get_nodes(flat=True)
                 VB = sum(X * dot(A, X))
                 VB = VB.reshape(self._grid.get_number_nodes())
-                prop = exp(-1.0j / self._eps**2 * VB) # ev. -0.5j durch -1j ersetzen...
+                #define the magnetic field propagator \Phi_B = \int{ -i/eps^2 * \int{ B^2}}
+                prop = exp(-1.0j / self._eps**2 * VB)
 
+                #apply the propagator
                 values = self._psi.get_values()
                 values = [prop * component for component in values]
 
+                #define the propagator \Phi_{\phi} (for electric and spatial potential)
+                # these potential are not independent of rotations and need to be rotated
                 self._potential.calculate_exponential(-1.0j *  self._a[i]*self._dt /self._eps**2)
 
+                # rotate and evaluate potentials
                 self._grid.rotate(R.T)
                 VE = self._potential.evaluate_exponential_at(self._grid)
                 self._VE = tuple([ve.reshape(self._grid.get_number_nodes()) for ve in VE])
 
-                #apply it
+                # apply the propagator
                 values = [self._VE * component for component in values]
                 self._grid.rotate(R)
 
+                # define time step for Magnus Integrator
                 t_interval[0] = t_b
                 t_interval[1] = t_b + self._b[i]*self._dt
 
+                # calculate Magnus Integrator
                 U = (FourierMagneticPropagator._Magnus_CF4(t_interval, B, 1)).T
                 R = dot(R , U)
                 if(R.shape != U.shape):
                     raise ValueError("Shapes of R and U do not match")
 
-                #check for obsolete splitting steps
+                # check for obsolete splitting steps
                 if(self._b[i] != 0):
+                    ### calculate kinetic flow map \Phi_{-\Delta} ###
+
+                    #go to fourier space
                     values = [fftn(component) for component in values]
 
-                    # Apply the kinetic operator
+                    # calculate the kinetic operator
                     self._KO = KineticOperator(self._grid, self._eps)
                     self._KO.calculate_exponential(-0.5j * self._eps**2 * self._b[i]*self._dt)
 
+                    #calculate and apply the kinetic propagator
                     TE = self._KO.evaluate_exponential_at()
                     values = [TE * component for component in values]
 
                     # Go back to real space
                     values = [ifftn(component) for component in values]
 
-                #Apply
+                # save data
                 self._psi.set_values(values)
 
-                #update t_a and t_b
+                #update time grids
                 t_a = t_a + self._a[i]*self._dt
                 t_b = t_b + self._b[i]*self._dt
 
